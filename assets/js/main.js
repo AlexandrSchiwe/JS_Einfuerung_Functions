e569fd6b00c19707861dbdd24789a4ed48981551
githubsupercode <alex.supercode@gmail.com>
2022-04-14 13:22:09 +0200

    lastpush

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+//  Functions Level 1.1
+
+// let a = 1 + 3;
+
+// function intro(a) {
+//     console.log(a);
+//     console.log("Hello World");
+// };
+
+// intro("1+3 = " + a)
+
+// Functions Level 1.2
+
+// let varName = "SuperCoder";
+
+// function intro2(paramName) {
+//     console.log("Hi, " + varName + ". Mein Name ist " + paramName + " .")
+// }
+
+// intro2("Alex");
+
+// Functions 1.3
+
+// function intro3(name, stadt, alter){
+//     console.log("Hallo, mein name ist " + name + " ." + " Ich bin " + alter + " Jahre alt. Ich komme aus " + stadt + ".");
+// }
+
+// intro3("Alex", "Düsseldorf" ,"28");
+
+// Functions 1.5
+
+// function math(a,b){
+//     console.log(a * b);
+// };
+
+// math(10, 2);
+
+
+// function math(a,b){
+//     console.log(a / b);
+// };
+
+// math(10, 2);
+
+// function math(a,b){
+//     console.log(a / b);
+// };
+
+// math(30, 20);
+
+// function math(a,b){
+//     console.log(a * b);
+// };
+
+// math(30, 20);
+
+// function math(a,b){
+//     console.log(a * b);
+// };
+
+// math(100, 100);
+
+// function math(a,b){
+//     console.log(a / b);
+// };
+
+// math(45, 173);
+
+function math(a,b){
+    console.log(a * b);
+};
+
+math(1, 1000);
